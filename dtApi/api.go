546b452c132a195c8ce1b330fc50df596ad67ff4
doll94av <45698606+doll94av@users.noreply.github.com>
2020-01-27 22:51:37 -0500

    More functionality and direction
    Added a struct for the API calls, and figured out that a switch is probably a good idea for the API urls

--- dtApi/api.go
+++ dtApi/api.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net/http"
 	"io/ioutil"
-	"log"
-	"encoding/json"
+	"net/http"
+	"os"
 )
 
-func responseBodyToString(r *http.Response) string{
-	
+type callSetup struct {
+	url string
+	method string
+}
+
+func responseBodyToString(r *http.Response) string {
+
 	//read the body bytes then load it into a string for easy management
 	bodyBytes, _ := ioutil.ReadAll(r.Body)
 	bodyString := string(bodyBytes)
@@ -17,25 +22,47 @@ func responseBodyToString(r *http.Response) string{
 	return bodyString
 }
 
-
+//we want to call the client with the desired API call and return responseBodyToString with the resposne
+//Create a http client, then do the request passed in the args, send the response to the toString and return
+func callApi(req *http.Request) string {
+	client := &http.Client{}
+	res, _ := client.Do(req)
+	return responseBodyToString(res)
+}
 
 func main() {
 
+	//default values for the API
 	var apiToken = "Api-Token dwi9bxPFTA-BlAf6aaWv8"
 	var apiURL = "https://rwn06871.dev.dynatracelabs.com/api/v1/entity/infrastructure/hosts?includeDetails=true"
 	var reqType = "GET"
-	//create http client for requests
-	client := &http.Client{}
+
+	//stdin reader
+	reader := bufio.NewReader(os.Stdin)
 
 	//set headers for requests and URL / request type
 	req, _ := http.NewRequest(reqType, apiURL, nil)
 	req.Header.Set("Authorization", apiToken)
-	res, _ := client.Do(req)
-	resString := responseBodyToString(res)
 
+	responseString := callApi(req)
+	fmt.Println(responseString)
 
-	fmt.Println(res)
-	fmt.Println("---------------------------------------------------------------")
-	fmt.Println(resString)
+	fmt.Println("Press 1 for Hosts, infrastructure or 2 for cluster time")
+	text, _ := reader.ReadString('\n')
+	
+	//idk it was getting mad at me for not declaring it
+	var callsetup = callSetup{"NIL", "NIL"}
 
+	//switch for cases (api calls), eventually this will read in from a frontend instead of cmdline
+	switch call := text; call {
+	case "1\n":
+	callsetup = callSetup{"https://rwn06871.dev.dynatracelabs.com/api/v1/entity/infrastructure/hosts?includeDetails=true", "GET"}
+	case "2\n":
+	callsetup = callSetup{"https://rwn06871.dev.dynatracelabs.com/api/v1/time", "GET"}
+	}
+
+	req, _ = http.NewRequest(callsetup.method, callsetup.url, nil)
+	req.Header.Set("Authorization", apiToken)
+	responseString = callApi(req)
+	fmt.Println(responseString)
 }
